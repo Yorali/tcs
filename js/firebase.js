a80221696c14c42ff31ef18d63f0532bd61e9099
thomasbyoung <thomasbyoung12345@gmail.com>
2018-05-03 17:20:54 -0500

    chimp

---
+++ js/firebase.js
@@ -0,0 +1,38 @@
+ 
+$(document).ready(function(){ 
+	
+
+
+    console.log('hello world');
+    
+    
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyB1ahHUoQMbjw-mzUhVGvPpjMq0UPmK8P4",
+        authDomain: "tcsemail-a7898.firebaseapp.com",
+        databaseURL: "https://tcsemail-a7898.firebaseio.com",
+        projectId: "tcsemail-a7898",
+        storageBucket: "tcsemail-a7898.appspot.com",
+        messagingSenderId: "802172603686"
+      };
+      firebase.initializeApp(config);
+    
+    
+    var database = firebase.database();
+    
+    var emailInput = '';
+    
+    $("#submit").on("click", function send() {
+    
+            nameInput = $("#mc-email").val().trim();
+
+    
+            database.ref().push({
+            name: emailInput
+    
+        });
+            $('#mod').empty().append(`<div class="rsvpHead"> thanks! </div>`);
+     });
+    
+    
+    });
