2947c5793b205e2580ea9ad330a8a43b2b93f9fa
thomasbyoung <thomasbyoung12345@gmail.com>
2018-05-07 10:58:31 -0500

    mail chimp config

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        mailChimpURL   : 'totalcannabissolution.us18.list-manage.com/subscribe/post?u=9592b8192b1c85c7e1dc63417&amp;id=af43f9c7da'   // mailchimp url
     },
 
     $WIN = $(window);
