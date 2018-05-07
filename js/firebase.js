2c1e1a93a03cae1a80555ce2b7c801e9bfd9331f
thomasbyoung <thomasbyoung12345@gmail.com>
2018-05-07 11:34:54 -0500

    turn off firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,38 +1,38 @@
  
-$(document).ready(function(){ 
+// $(document).ready(function(){ 
 	
 
 
-    console.log('hello world');
+//     console.log('hello world');
     
     
-      // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyB1ahHUoQMbjw-mzUhVGvPpjMq0UPmK8P4",
-        authDomain: "tcsemail-a7898.firebaseapp.com",
-        databaseURL: "https://tcsemail-a7898.firebaseio.com",
-        projectId: "tcsemail-a7898",
-        storageBucket: "tcsemail-a7898.appspot.com",
-        messagingSenderId: "802172603686"
-      };
-      firebase.initializeApp(config);
+//       // Initialize Firebase
+//       var config = {
+//         apiKey: "AIzaSyB1ahHUoQMbjw-mzUhVGvPpjMq0UPmK8P4",
+//         authDomain: "tcsemail-a7898.firebaseapp.com",
+//         databaseURL: "https://tcsemail-a7898.firebaseio.com",
+//         projectId: "tcsemail-a7898",
+//         storageBucket: "tcsemail-a7898.appspot.com",
+//         messagingSenderId: "802172603686"
+//       };
+//       firebase.initializeApp(config);
     
     
-    var database = firebase.database();
+//     var database = firebase.database();
     
-    var emailInput = '';
+//     var emailInput = '';
     
-    $("#submit").on("click", function send() {
+//     $("#submit").on("click", function send() {
     
-            nameInput = $("#mc-email").val().trim();
+//             nameInput = $("#mc-email").val().trim();
 
     
-            database.ref().push({
-            name: emailInput
+//             database.ref().push({
+//             name: emailInput
     
-        });
-            $('#mod').empty().append(`<div class="rsvpHead"> thanks! </div>`);
-     });
+//         });
+//             $('#mod').empty().append(`<div class="rsvpHead"> thanks! </div>`);
+//      });
     
     
-    });
+//     });
